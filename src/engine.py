3d4b5f5f62394dbbaa1cbab80f05c733b51033c8
rahulkp-ai <rahulkpkurup@gmail.com>
2026-05-27 10:19:06 +0530

    feat: add Value class for automatic differentiation

---
+++ src/engine.py
@@ -0,0 +1,135 @@
+import math
+
+class Value:
+
+    def __init__(self, data, children=(), _op='', label=''):
+        self.data = float(data)
+        self.grad = 0.0
+
+        self.backward = lambda: None
+        self._prev = set(children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data:.4f},grad={self.grad:.4f})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(oter)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
+        return out
+
+    def __radd__(self, other):
+        return self + other
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.grad * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def __rmul__(self,other):
+        return self*other
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return Value(other) + (-self)
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __rtruediv__(self,other):
+        return Value(other) * self**-1
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Exponent must be int or float"
+        out = Value(self.data**other, (self,), f'**{other}')
+        
+        def _backward():
+            self.grad += other * (self.data**(other - 1)) * out.grad
+
+        out._backward = _backward
+        return out
+
+    def tanh(self):
+        t = (math.exp(2 * self.data) - 1) / (math.exp(2 * self.data) + 1)
+        out = Value(t, (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        out = Value(max(0, self.data), (self,), 'relu')
+
+        def _backward():
+            self.grad += (1.0 if self.data > 0 else 0.0) * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def sigmoid(self):
+        s = 1 / (1 + math.exp(-self.data))
+        out = Value(s, (self,), 'sigmoid')
+
+        def _backward():
+            self.grad += s * (1 - s) * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def exp(self):
+        out = Value(math.exp(self.data), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+        return out
+
+    def log(self):
+        assert self.data > 0, "log is only defined for positive valuses"
+        out = Value(math.log(self.data), (self,), 'log')
+
+        def _backward():
+            self.grad += (1 / self.data) * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build(child)
+                topo.append(v)
+        
+        build(self)
+
+        self.grad = 1.0
+
+        for node in reversed(topo):
+            node.backward()
+        
